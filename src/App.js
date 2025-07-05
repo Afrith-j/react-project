c0d3aef513a755d1726eb8552dab59242b75cec0
afrit <afrithmohammed13@gmail.com>
2025-07-05 21:48:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://reactjs.org">Learn React</a>
+		<Login/>
+    </div>
+  );
+}
+
+export default App;
